a144c49050b3abeb783580ff30245bbbfb77da98
Amber Taranto <ambzzz@gmail.com>
2020-06-09 19:15:31 -0700

    seeded some data

--- travel-wish-list-backend/db/seeds.rb
+++ travel-wish-list-backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Itinerary.create(name: 'Santa Cruz')
+Itinerary.create(name: 'Los Angeles')
+Itinerary.create(name: 'New Mexico')
