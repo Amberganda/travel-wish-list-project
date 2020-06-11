8d9152deff2cd66a7b9b2d2999883c1ed100d852
Amber Taranto <ambzzz@gmail.com>
2020-06-10 17:13:37 -0700

    added button icon

--- travel-wish-list-frontend/src/index.js
+++ travel-wish-list-frontend/src/index.js
@@ -1,4 +1,5 @@
 const BACKEND_URL = 'http://localhost:3000';
+// 
 
 document.addEventListener("DOMContentLoaded", function () {
     fetch(`${BACKEND_URL}/itineraries`)
@@ -9,10 +10,27 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 function appendItinerary(itinerary) {
-    const div = document.createElement("div")
-    div.innerHTML = itinerary.name
-    div.classList.add("card", "grey")
+    //create div w/ Itinerary Names
+    const div = document.createElement('div')
+    div.classList.add("card", "indigo", "accent-2")
 
+    // make p tag
+    const title = document.createElement('p')
+    title.innerText = itinerary.name
+    div.appendChild(title)
+
+    //make button
+    const button = document.createElement("a")
+    button.classList.add("btn-floating", "halfway-fab", "waves-effect", "waves-light", "red")
+    div.appendChild(button)
+
+    const addIcon = document.createElement('i')
+    addIcon.classList.add("material-icons")
+    addIcon.innerText = "add"
+    button.appendChild(addIcon)
+    // a class = "btn-floating halfway-fab waves-effect waves-light red" > < i class = "material-icons" > add < /i></a >
+
+    //build me up buttercup
     document.body.appendChild(div)
 
 
